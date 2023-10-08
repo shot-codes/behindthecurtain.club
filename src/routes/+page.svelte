28d758bea3f02999ffe2a38eacd2b9898cc02667
Nicolai <nicolai.verbaarschot@gmail.com>
2023-10-08 23:56:37 +0200

    Initial commit.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div class="flex flex-col w-full h-full items-center justify-center">
+	<img src="/images/behindthecurtain.png" alt="behind the curtain logo" class="w-[300px] m-3" />
+	<div class="flex flex-col justify-center text-center mt-6">
+		<p>Åboulebard 10</p>
+		<p>2200 Copenhagen N</p>
+		<p>Denmark</p>
+	</div>
+</div>
