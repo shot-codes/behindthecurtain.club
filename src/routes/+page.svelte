a5b79cebf6839448439966e0aff0855c7c748c12
N <contact@shot.codes>
2023-10-22 16:13:24 +0200

    typo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="flex flex-col w-full h-full items-center justify-center">
 	<img src="/images/behindthecurtain.png" alt="behind the curtain logo" class="w-[300px] m-3" />
 	<div class="flex flex-col justify-center text-center mt-6">
-		<p>Åboulebard 10</p>
+		<p>Åboulevard 10</p>
 		<p>2200 Copenhagen N</p>
 		<p>Denmark</p>
 	</div>
